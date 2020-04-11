2047984971d255701bbc5e0d9fdc2984a17a97d5
vagrant <vagrant@homestead>
2020-04-11 07:54:45 +0000

    微信支付分期还款

--- routes/web.php
+++ routes/web.php
@@ -61,6 +61,7 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('installments/{installment}', 'InstallmentsController@show')->name('installments.show');
     Route::get('installments/{installment}/alipay', 'InstallmentsController@payByAlipay')->name('installments.alipay');
     Route::get('installments/alipay/return', 'InstallmentsController@alipayReturn')->name('installments.alipay.return');
+    Route::get('installments/{installment}/wechat', 'InstallmentsController@payByWechat')->name('installments.wechat');
 });
 
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
@@ -70,4 +71,5 @@ Route::post('payment/wechat/refund_notify', 'PaymentController@wechatRefundNotif
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 Route::post('installments/alipay/notify', 'InstallmentsController@alipayNotify')->name('installments.alipay.notify');
+Route::post('installments/wechat/notify', 'InstallmentsController@wechatNotify')->name('installments.wechat.notify');
 
